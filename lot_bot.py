b5cdb3eb60f057bda0309c196489eedcaae7ad49
AndyMissed <andrew.mccastle@gmail.com>
2020-06-03 03:56:40 -0400

    Added embed functionality
    Bot now embeds its text, which looks prettier.

--- lot_bot.py
+++ lot_bot.py
@@ -1,5 +1,5 @@
 # Code for Luck-o-Tron Discord bot
-# Version 1.0.1
+# Version 1.1.0
 # Copyright © 2020 AndyMissed
 
 import os
@@ -19,6 +19,7 @@ async def on_ready():
   print('Luck-o-Tron is online.')
 
 bot.remove_command('help')
+
 
 # Takes a list of strings (str_list) and combines them together in a formatted manner.
 def align(str_list):
@@ -42,6 +43,7 @@ def align(str_list):
   output = '\n'.join(output)
   return output
 
+
 # Takes an integer (number) and another integer (num_max).
 # Outputs [number] as a string, and adds spaces based on missing digits in comparison to [num_max].
 # [num_max] should always be higher than [number].
@@ -57,6 +59,7 @@ def add_gaps(number, num_max):
     numgaps = max_len - num_len
     output = (' '*numgaps)+num_str
     return output
+
 
 # 'Rolls' a die with a specified number of sides (sides) and outputs a random number.
 # Luck is an essential argument to set the behavior of randomness.
@@ -88,48 +91,85 @@ def rng(sides, luck):
     elif luck < 0:
       return (sides+1)-output  # Hacky code that 'inverts' the result for negative values of luck.
 
+
+def convert(string):
+  output = discord.Embed(
+    description=string
+    )
+  return output
+
+
 @bot.command()
 async def help(chat, word = str()):
-  s = '<---------->'  # Separator
-  help_desc = (
-    'Commands:\n'+
-    f'{s}\n'+
-    '"d20":\n'+
-    'Rolls a 20-sided die.\n'+
-    '"roll":\n'+
-    'Rolls a die with a specified amount of sides. Defaults to 6 if no sides specified.\n'+
-    f'{s}\n'+
-    'Features:\n'+
-    f'{s}\n'+
-    'luck:\n'+
-    'Controls the randomness of a roll.\n'+
-    f'{s}\n\n'+
-    'For more information about a specific command or feature, use:\n'+
-    '"%help [command/feature]"\n\n'+
-    'Example command:\n'+
-    '"%roll 25 10 20"'
-    )
-  luck_desc = (
-    f'Description:\n{s}\nLuck is a value that modifies the randomness of a roll.\n\n'+
-    'If the value of luck is 0 (Default), rolls behave normally.\n\nAs the value of luck increases '+
-    '(Up to a maximum of [the # of sides]), low numbers become rolled less frequently.\n\n'+
-    'As the value of luck decreases, high numbers become rolled less frequently.\n'+
-    '(It should be noted that luck can go negative down to a minimum of [the # of sides, but negative])\n\n'+
-    'In essence, your average roll gets lower with lower luck, and gets higher with higher luck.\n\n'+
-    'As an example, if you have a d20 with 20 luck (maximum luck), the odds of rolling a 1 '+
-    'is essentially reduced to 1 out of every 8000 rolls.\n'+
-    f'{s}'
-    )
   if word == 'd20':
-    await chat.send(f'```Usage:\n{s}\nd20 [# of dice] [# of luck]\n{s}```')
+    msg = discord.Embed(
+      title='Usage',
+      description='`%d20 [# of dice] [# of luck]`'
+      )
+    await chat.send(embed=msg)
   elif word == 'roll':
-    await chat.send(f'```Usage:\n{s}\nroll [# of sides] [# of dice] [# of luck]\n{s}```')
+    msg = discord.Embed(
+      title='Usage',
+      description='`%roll [# of sides] [# of dice] [# of luck]`'
+      )
+    await chat.send(embed=msg)
   elif word == 'luck':
-    await chat.send(f'```{luck_desc}```')
+    msg = discord.Embed(
+      title='Luck',
+      description=
+      'Luck is a value that modifies the randomness of a roll.\n\n'+
+      'If the value of luck is 0 (Default), rolls behave normally.\n\n'+
+      'As the value of luck increases (Up to a maximum of [the # of sides]), '+
+      'low numbers become rolled less frequently.\n\n'+
+      'As the value of luck decreases, '+
+      'high numbers become rolled less frequently.\n'+
+      '(It should be noted that luck can go negative down to '+
+      'a minimum of [the # of sides, but negative])\n\n'+
+      'In essence, your average roll gets lower with lower luck, '+
+      'and gets higher with higher luck.\n\n'+
+      'As an example, if you have a d20 with 20 luck (maximum luck), '+
+      'the odds of rolling a 1 is essentially reduced to 1 out of every 8000 rolls.'
+      )
+    await chat.send(embed=msg)
+  elif word == 'features':
+    msg = discord.Embed(
+      title='Features'
+      )
+    msg.add_field(
+      name='`luck`',
+      value='Controls the randomness of a roll.',
+      inline=False
+      )
+    msg.set_footer(
+      text='Use "%help [feature]" for more information.'
+      )
+    await chat.send(embed=msg)
   elif word != '':
-    await chat.send(f'```"{word}" is not a recognized command or feature.```')
+    msg = convert(f'`{word}` is not a recognized command or feature.')
+    await chat.send(embed=msg)
   else:
-    await chat.send(f'```{help_desc}```')
+    msg = discord.Embed(
+      title='Commands'
+      )
+    msg.add_field(
+      name='`%d20`',
+      value='Rolls a 20-sided die.',
+      inline=False
+      )
+    msg.add_field(
+      name='`%roll`',
+      value=
+      'Rolls a die with a specified amount of sides.\n'+
+      'Defaults to 6 if no sides specified.',
+      inline=False
+      )
+    msg.set_footer(
+      text=
+      'Use "%help [command]" for more information.\n'+
+      'Use "%help features" for information about features.'
+      )
+    await chat.send(embed=msg)
+
 
 @bot.command()
 async def d20(chat, n_dice: str = '1', n_luck: str = '0'):
@@ -137,24 +177,32 @@ async def d20(chat, n_dice: str = '1', n_luck: str = '0'):
   try:
     n_dice = int(n_dice)
   except ValueError:
-    await chat.send(f'```"{n_dice}" is not a valid number.```')
+    msg = convert(f'`{n_dice}` is not a valid number.')
+    await chat.send(embed=msg)
     error = 1
   try:
     n_luck = int(n_luck)
   except ValueError:
-    await chat.send(f'```"{n_luck}" is not a valid number.```')
+    msg = convert(f'`{n_luck}` is not a valid number.')
+    await chat.send(embed=msg)
     error = 1
+    
   if error == 0:
     if not -20 <= n_luck <= 20 or not 1 <= n_dice <= 100:
       if not -20 <= n_luck <= 20:
-        await chat.send("```Luck must be within −20 to 20.```")
+        msg = convert('Luck must be within −20 to 20.')
+        await chat.send(embed=msg)
       if n_dice > 100:
-        await chat.send("```Dice must be within 1 to 100.```")
+        msg = convert('Dice must be within 1 to 100.')
+        await chat.send(embed=msg)
       if n_dice == 0:
-        await chat.send("```No dice. Sorry.```")
+        msg = convert('No dice. Sorry.')
+        await chat.send(embed=msg)
       if n_dice < 0:
-        await chat.send("```In Soviet Russia, dice throw *you*.```")
+        msg = convert('In Soviet Russia, dice throw *you*.')
+        await chat.send(embed=msg)
       error = 1
+      
     if error == 0:
       roll = []
       total = 0
@@ -162,10 +210,19 @@ async def d20(chat, n_dice: str = '1', n_luck: str = '0'):
         num = rng(20, n_luck)
         total += num
         roll.append(add_gaps(num, 20))
+        
+      msg = discord.Embed(
+        title='Result',
+        description=f'{align(roll)}',
+        color=0xf0f0f0
+        )
       if n_dice > 1:
-        await chat.send(align(roll)+str(f"\n`Total: {total}`"))
-      else:
-        await chat.send(align(roll))
+        msg.title = 'Results'
+        msg.set_footer(
+          text=f'Total: {total}'
+          )
+      await chat.send(embed=msg)
+
 
 @bot.command()
 async def roll(chat, n_sides: str = '6', n_dice: str = '1', n_luck: str = '0'):
@@ -174,37 +231,49 @@ async def roll(chat, n_sides: str = '6', n_dice: str = '1', n_luck: str = '0'):
   try:
     n_sides = int(n_sides)
   except ValueError:
-    await chat.send(f'```"{n_sides}" is not a valid number.```')
+    msg = convert(f'`{n_sides}` is not a valid number.')
+    await chat.send(embed=msg)
     error = 1
   try:
     n_dice = int(n_dice)
   except ValueError:
-    await chat.send(f'```"{n_dice}" is not a valid number.```')
+    msg = convert(f'`{n_dice}` is not a valid number.')
+    await chat.send(embed=msg)
     error = 1
   try:
     n_luck = int(n_luck)
   except ValueError:
-    await chat.send(f'```"{n_luck}" is not a valid number.```')
+    msg = convert(f'`{n_luck}` is not a valid number.')
+    await chat.send(embed=msg)
     error = 1
+    
   if error == 0:
-    if not n_sides*-1 <= n_luck <= n_sides or not 1 <= n_sides <= 9999 or not 1 <= n_dice <= 100:
-      if (n_luck > n_sides or n_luck < (n_sides*-1)) and n_sides > 1:
-        await chat.send(f"```Luck must be within −{n_sides} to {n_sides}.```")
+    if not (n_sides*-1) <= n_luck <= n_sides or not 1 <= n_sides <= 9999 or not 1 <= n_dice <= 100:
+      if not ((n_sides*-1) <= n_luck <= n_sides) and n_sides > 1:
+        msg = convert(f'Luck must be within −{n_sides} to {n_sides}.')
+        await chat.send(embed=msg)
       if n_sides > 9999:
-        await chat.send("```Sides must be within 2 to 9999.```")
+        msg = convert('Sides must be within 2 to 9999.')
+        await chat.send(embed=msg)
       if n_sides == 0:
-        await chat.send("```You can't have zero sides; You'd break the universe.```")
+        msg = convert('You can\'t have zero sides; You\'d break the universe.')
+        await chat.send(embed=msg)
       if n_sides < 0:
-        await chat.send("```Negative sides? Don't be silly.```")
+        msg = convert('Negative sides? Don\'t be silly.')
+        await chat.send(embed=msg)
       if n_dice > 100:
-        await chat.send("```Dice must be within 1 to 100.```")
+        msg = convert('Dice must be within 1 to 100.')
+        await chat.send(embed=msg)
       if n_dice == 0:
-        await chat.send("```No dice. Sorry.```")
+        msg = convert('No dice. Sorry.')
+        await chat.send(embed=msg)
       if n_dice < 0:
-        await chat.send("```In Soviet Russia, dice throw *you*.```")
+        msg = convert('In Soviet Russia, dice throw *you*.')
+        await chat.send(embed=msg)
       error = 1
     if n_sides == 1:  # Why has the user done this?
       pointless = 1
+      
     if error == 0:
       roll = []
       total = 0
@@ -212,16 +281,20 @@ async def roll(chat, n_sides: str = '6', n_dice: str = '1', n_luck: str = '0'):
         num = rng(n_sides, n_luck)
         total += num
         roll.append(add_gaps(num, n_sides))
-      if pointless == 1:  # If you insist...
-        text = (
-          "```Why would you even want just one side?\n"+
-          "Whatever, dude. Here are the results...```"
-          )
-      else:
-        text = ''
+        
+      msg = discord.Embed(
+        title='Result',
+        description=f'{align(roll)}',
+        color=0xf0f0f0
+        )
       if n_dice > 1:
-        await chat.send(text+align(roll)+str(f"\n`Total: {total}`"))
-      else:
-        await chat.send(text+align(roll))
+        msg.title = 'Results'
+        msg.set_footer(
+          text=f'Total: {total}'
+          )
+      if pointless == 1:  # If you insist...
+        msg.title = 'Seriously?'
+      await chat.send(embed=msg)
+
 
 bot.run(TOKEN)
